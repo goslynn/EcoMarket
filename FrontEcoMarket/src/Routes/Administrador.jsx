17a19fb16a383488ce9044a90966eb9a94fd5efd
dwigthDev <slopez@grupodmc.cl>
2025-05-14 16:13:44 -0400

    la mochila hizo cambios en el fron

---
+++ FrontEcoMarket/src/Routes/Administrador.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideBar from '../Componentes/SideBar'
+
+
+const Administrador = () => {
+  return (
+        <SideBar></SideBar> 
+        
+  )
+}
+
+export default Administrador
