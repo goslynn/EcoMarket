0b7bc6da44a6e5ff54418059c467e228793345b0
goslynn <vct.gonzaleza@gmail.com>
2025-06-13 23:48:54 -0400

    Agregando logging en puntos claves de auth

--- src/main/java/cl/duocuc/ecomarket/servicio/ServicioAuth.java
+++ src/main/java/cl/duocuc/ecomarket/servicio/ServicioAuth.java
@@ -3,7 +3,6 @@ package cl.duocuc.ecomarket.servicio;
 import cl.duocuc.ecomarket.funcional.PermisoFuncional;
 import cl.duocuc.ecomarket.modelo.dto.usuario.LoginRequestDTO;
 import cl.duocuc.ecomarket.modelo.entity.usuario.Permiso;
-import cl.duocuc.ecomarket.modelo.entity.usuario.Rol;
 import cl.duocuc.ecomarket.modelo.entity.usuario.Usuario;
 import cl.duocuc.ecomarket.security.EcomarketJWT;
 import cl.duocuc.ecomarket.security.ProveedorJWT;
@@ -11,9 +10,9 @@ import cl.duocuc.ecomarket.tipodatos.TipoPermiso;
 import cl.duocuc.ecomarket.util.CodigoDescripcion;
 import cl.duocuc.ecomarket.util.encriptacion.Encriptador;
 import cl.duocuc.ecomarket.util.exception.ApiException;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -23,6 +22,7 @@ public class ServicioAuth {
     private final ServicioUsuarios servicioUsuarios;
     private final ProveedorJWT jwt;
     private final Encriptador<String> encriptador;
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(ServicioAuth.class);
 
     public ServicioAuth(ServicioUsuarios servicioUsuarios, EcomarketJWT jwt, Encriptador<String> encriptador) {
         this.servicioUsuarios = servicioUsuarios;
@@ -36,10 +36,12 @@ public class ServicioAuth {
                 .filter(Usuario::getActivo)
                 .orElseThrow(() -> new ApiException(404, "Usuario no encontrado con el correo proporcionado: " + dto.correo()));
 
+        log.info("Procesando login para usuario: {} - {}", u.getId(), u.getNombreUsuario() );
         if (!(encriptador.desencriptar(u.getContrasenaHash())).equals(dto.contrasena())) {
             throw new ApiException(401, "Contrasena incorrecta!");
         }
 
+        log.info("Usuario {} - {} autenticado correctamente", u.getId(), u.getNombreUsuario() );
         return CodigoDescripcion.of(
                 u.getId(),
                 jwt.generarToken(u.getId(), u.getRol().getId())
