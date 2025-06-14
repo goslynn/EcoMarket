411be4082c65b3ce7d7d84f6782c359f7bb6666f
goslynn <vct.gonzaleza@gmail.com>
2025-06-13 22:32:58 -0400

    Empezando desarrollo de pruebas unitarias

---
+++ src/test/java/cl/duocuc/ecomarket/test/EcomarketTest.java
@@ -0,0 +1,11 @@
+package cl.duocuc.ecomarket.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EcomarketTest {
+
+    @Test
+    void contextLoads(){}
+}
